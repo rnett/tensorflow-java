2bfd55b54c78656fbe75b029bbb65d635a4a7c28
Jim Clarke <JimClarke5@me.com>
2021-04-05 12:59:28 -0400

    Merge from rnett.

--- tensorflow-core/tensorflow-core-generator/src/main/java/org/tensorflow/op/generator/ClassGenerator.java
+++ tensorflow-core/tensorflow-core-generator/src/main/java/org/tensorflow/op/generator/ClassGenerator.java
@@ -230,6 +230,8 @@ final class ClassGenerator {
     String summary = parseDocumentation(apiDef.getSummary());
     if (!summary.isEmpty()) {
       builder.addJavadoc("$L", summary + "\n");
+    } else {
+      builder.addJavadoc("$L", String.format("The %s operation\n", apiDef.getGraphOpName()));
     }
     String desc = parseDocumentation(apiDef.getDescription());
     if (!desc.isEmpty()) {
@@ -473,7 +475,11 @@ final class ClassGenerator {
       String name = getJavaName(input);
 
       ParameterSpec.Builder param = ParameterSpec.builder(type.iterableIfIterable().javaType, name);
-      paramTags.put(name, CodeBlock.of("$L\n", parseDocumentation(argDef.getDescription())));
+      String description =
+          argDef.getDescription().isEmpty()
+              ? String.format("the %s value", name)
+              : argDef.getDescription();
+      paramTags.put(name, CodeBlock.of("$L\n", parseDocumentation(description)));
       factoryBuilder.addParameter(param.build());
 
       typeVars.addAll(type.findGenerics());
@@ -499,9 +505,15 @@ final class ClassGenerator {
       ResolvedType type = resolver.typeOf(attr);
       ApiDef.Attr apiAttr = attrApis.get(attr);
 
-      ParameterSpec.Builder builder = ParameterSpec
-          .builder(type.classIfGeneric().listIfIterable().javaType, getJavaName(attr));
-      paramTags.put(getJavaName(attr), CodeBlock.of("$L\n", parseDocumentation(apiAttr.getDescription())));
+      ParameterSpec.Builder builder =
+          ParameterSpec.builder(type.classIfGeneric().listIfIterable().javaType, getJavaName(attr));
+      String javaName = getJavaName(attr);
+      String description =
+          apiAttr.getDescription().isEmpty()
+              ? String.format("the value of the %s property", javaName)
+              : apiAttr.getDescription();
+
+      paramTags.put(javaName, CodeBlock.of("$L\n", parseDocumentation(description)));
 
       typeVars.addAll(type.findGenerics());
 
